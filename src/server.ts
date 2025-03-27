28ba5da1128fbe0be41b1cef45a72251eae5282f
Henrique Aguiar <henriqueaguiarpacheco09@gmail.com>
2025-03-27 16:47:57 -0300

    Configurado o servidor em typescript

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const server = express();
+
+// como a função esta diretamente dentro do get o express ja identifica os tipos de cada parametro do callback
+server.get("/home", (request, reply) => {
+  const info: string = "Hello World!";
+  reply.send(info);
+});
+
+server.listen({
+    port: 3000  
+});
+
+
+
+//Tambem podemos usar desta forma a definição  das rotas criando uma variavel controller.
+// Porem neste caso o Typescript sempre pede os tipos das variaveis da função callback  
+// ou seja os tipos da biblioteca do express observar bem
+/* 
+
+import express from 'express';
+
+const server = express();
+
+const controller = (request: Request, reply: Response) => {
+  return reply.send("Hello World!");
+}
+servidor.get("/home", controller);
+
+server.listen({
+    port: 3000  
+});
+
+
+*/
